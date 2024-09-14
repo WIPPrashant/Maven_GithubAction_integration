111860e6ea71b5607c45248f2a1f5708686bf31e
RaPr020 <RaPr020@HP-5CG8283BFM>
2024-09-14 22:37:35 +0530

    First commit to check https://github.com/WIPPrashant/Maven_GithubAction_integration.git

---
+++ src/main/java/test/Maven_GithubAction_integration/App.java
@@ -0,0 +1,13 @@
+package test.Maven_GithubAction_integration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/test/Maven_GithubAction_integration/WebTest1.java
@@ -0,0 +1,22 @@
+package test.Maven_GithubAction_integration;
+
+import org.testng.annotations.Test;
+
+public class WebTest1 {
+	
+	@Test(priority=1)
+	public void Test1() {
+		System.out.println("test case passed");
+	}
+	
+	@Test(priority=2)
+	public void Test2() {
+		System.out.println("test case fail");
+	}
+	
+	@Test(priority=3)
+	public void Test3() {
+		System.out.println("test case skiped");
+	}
+
+}
